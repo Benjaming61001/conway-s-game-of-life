ba6e685a4df801076e45034a35564b72c7fad3aa
Pavin Butprom <vin@makewebbkk.com>
2026-01-20 13:10:27 +0700

    created nuxt

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <Card class="min-h-dvh w-dvw grid place-items-center">
+    <template #content>
+      <slot />
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>
-    home
+  <div class="flex flex-col items-center gap-8">
+    <div class="bg-white flex flex-col">
+      <div
+        v-for="(row, xPosition) in data"
+        :key="`${xPosition}`"
+        class="flex">
+        <div
+          v-for="(column, yPosition) in row"
+          :key="`${xPosition}-${yPosition}`"
+          :class="{
+            'bg-black': column
+          }"
+          class="size-4 border border-slate-400"
+          @click="toggleCell(xPosition, yPosition)" />
+      </div>
+    </div>
+    <div class="flex gap-4 items-center">
+      <Button label="Button" />
+      <Button label="Button" />
+      <Button label="Button" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+const _SIZE = 50
+const data = ref<boolean[][]>([])
 
+function toggleCell (xPosition: number, yPosition: number): void {
+  if (data.value?.[xPosition]?.[yPosition] === undefined) return
+  data.value[xPosition][yPosition] = !data.value[xPosition][yPosition]
+}
+
+onMounted((): void => {
+  const result = data.value
+  for (let i = 0; i < _SIZE; i++) {
+    result.push([])
+    for (let j = 0; j < _SIZE; j++) {
+      result[i]!.push(false)
+    }
+  }
+})
 </script>
 
 <style scoped>
